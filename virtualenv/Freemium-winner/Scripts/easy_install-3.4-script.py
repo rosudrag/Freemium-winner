0e68ec56329f9ae711e4c7b536b1f2345f56f26f
Dragos Rosu <r.dragosh@yahoo.com>
2016-01-09 09:46:52 +0000

    feat: first commit, project setup

---
+++ virtualenv/Freemium-winner/Scripts/easy_install-3.4-script.py
@@ -0,0 +1,10 @@
+#!"D:\Development 2.0\Freemium-winner\virtualenv\Freemium-winner\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==18.1','console_scripts','easy_install-3.4'
+__requires__ = 'setuptools==18.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==18.1', 'console_scripts', 'easy_install-3.4')()
+    )
