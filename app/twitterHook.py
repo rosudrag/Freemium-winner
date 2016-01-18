11478be01a4aea86dee7de2156127757d68d4bc4
Dragos Rosu <r.dragosh@yahoo.com>
2016-01-18 21:54:04 +0000

    feat: twitter hook tests wip

--- app/twitterHook.py
+++ app/twitterHook.py
@@ -1,17 +1,18 @@
 from TwitterAPI import TwitterAPI
 from app.adminConstants import AdminConstants
 
+
 class TwitterHook():
     def __init__(self):
-        adminConstants = AdminConstants()
-        self.consumer_key = adminConstants.consumer_key
-        self.consumer_secret = adminConstants.consumer_secret
+        adminconstants = AdminConstants()
+        self.consumer_key = adminconstants.consumer_key
+        self.consumer_secret = adminconstants.consumer_secret
 
-    def CanLogin(self):
+    def can_login(self):
         # api = TwitterAPI(self.consumer_key, self.consumer_secret, self.access_token_key, self.access_token_secret)
         api = TwitterAPI(self.consumer_key, self.consumer_secret, auth_type="oAuth2")
         try:
-            r = api.request('search/tweets', {'q':'pizza'})
+            r = api.request('search/tweets', {'q': 'pizza'})
             if r.status_code == 200:
                 return True
         except:
--- appTests/TwitterHookTests.py
+++ appTests/TwitterHookTests.py
@@ -3,13 +3,11 @@ from app.twitterHook import TwitterHook
 
 
 class TwitterHookTests(unittest.TestCase):
-
     def test_twitterhook_exists(self):
         twitterhook = TwitterHook()
         self.assertIsNotNone(twitterhook)
 
-
     def test_twitterhook_can_login(self):
-        twitterHook = TwitterHook()
-        twitterHookResult = twitterHook.CanLogin()
-        self.assertEqual(True, twitterHookResult)
+        twitterhook = TwitterHook()
+        twitterhookresult = twitterhook.can_login()
+        self.assertEqual(True, twitterhookresult)
