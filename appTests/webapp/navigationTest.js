5a120b95eb150ed0b0a1fcf8e371ff09fb31a1fb
cristwelrine <cristwelrine@yahoo.com>
2016-01-27 21:41:51 +0000

    Angular Unit Tests Part 1 - broken

--- appTests/webapp/navigationTest.js
+++ appTests/webapp/navigationTest.js
@@ -14,6 +14,7 @@ describe('Unit testing navigation',function(){
         $rootScope.$digest();
     }));
 
+
     it('Should not be empty',function(){
         expect(element.html().not.toEqual(''));
     });
