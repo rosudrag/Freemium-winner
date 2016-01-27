5f569f3a31a37f77440df5c83803e8d1d66b77ba
cristwelrine <cristwelrine@yahoo.com>
2016-01-27 21:36:05 +0000

    Angular Unit Tests Part 1 - broken

---
+++ appTests/webapp/navigationTest.js
@@ -0,0 +1,27 @@
+describe('Unit testing navigation',function(){
+    var $compile,$rootScope,$httpBackend,template;
+
+    var element;
+    beforeEach(module('freemium','templates'));
+    beforeEach(inject(function(_$compile_,_$rootScope_,$templateCache){
+        template = $templateCache.get('app/templates/view/nav-bar.html');
+        $templateCache.put('/template/nav-bar.html',template);
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        //$httpBackend = $injector.get('$httpBackend');
+        //$httpBackend.expectGET('template/nav-bar.html').passThrough();
+        element = $compile("<navigation></navigation>")($rootScope);
+        $rootScope.$digest();
+    }));
+
+    it('Should not be empty',function(){
+        expect(element.html().not.toEqual(''));
+    });
+    it('Should have two div',function(){
+        expect(element.find('div').length).toEqual(2);
+    });
+
+    it('Should contain header text',function(){
+        expect(element.html()).toContain("Freemium Winner");
+    })
+});
