dfd88ae5aa380d14ee4b1044b11b30ed28d6689b
Dragos Rosu <r.dragosh@yahoo.com>
2016-02-17 16:51:09 +0000

    feat: added package requiremets for python and fixed index html

--- app/freemiumwebapp/__init__.py
+++ app/freemiumwebapp/__init__.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/template/main.html")
 def main():
     return render_template("view/main.html")
+
 
 @app.route("/template/ceva.html")
 def ceva():
     return render_template("view/ceva.html")
 
+
 def run_webserver():
     app.run()
+
 
 if __name__ == "__main__":
     app.debug = True
